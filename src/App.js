72e2c4b728883eccd69f7c64ddad0e22a8af1b18
Roberto Costa <robertocosta_96@outlook.com>
2022-04-27 22:15:30 -0300

    Utilizando State no Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Comentario from './components/Comentario';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Meu projeto</h1>
-     <Comentario nome="João" email="joao@mail.com" data={new Date(2020, 3, 19)}>
-       Olá, tudo bem?
-     </Comentario>
-     <Comentario nome="Maria" email="maria@mail.com" data={new Date(2020, 3, 21)}> 
-      Olá, tudo bem sim!
-     </Comentario>
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    comentarios: [
+      {
+        nome: 'João',
+        emai: 'joao@mail.com',
+        data: new DataTransfer(2020, 3, 19),
+        mensagem: 'Olá, tudo bem?'
+      },
+      {
+        nome: 'Pedro',
+        emai: 'pedro@mail.com',
+        data: new DataTransfer(2020, 3, 22),
+        mensagem: 'Olá, tudo bem sim....'
+      },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <h1>Meu projeto</h1>
+        
+        {this.state.comentarios.map((comentario, indice)  => (
+           <Comentario 
+           key={indice}
+           nome={comentario.nome} 
+           email= {comentario.email}
+           data={comentario.data}>
+           {comentario.mensagem}
+            </Comentario>
+        ))}  
+      </div>
+    );
+  }
+  
 }
 
 export default App;
